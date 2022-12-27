168220fa350d74cd20c39240cc89aee0c9b1d43e
Inaue <100319268+Inaue@users.noreply.github.com>
2022-12-27 14:58:07 +0000

    makiing directory 'sh' for exercises

---
+++ shell/sh/meeting.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo Digite seu nome:
+read NOME
+echo Digite seu idade:
+read IDADE
+echo Digite seu genero:
+read GENERO
+echo Nome: $NOME
+echo Idade: $IDADE
+echo Genero: $GENERO
+unset NOME
+unset IDADE
+unset GENERO
